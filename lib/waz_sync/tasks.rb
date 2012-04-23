4753d2b5afa6c80655d8afedc13973044f4fdfcb
Guillaume Montard <montard@vodeclic.com>
2012-04-23 14:05:42 +0200

    Adding progress to rake task

--- lib/waz_sync/tasks.rb
+++ lib/waz_sync/tasks.rb
@@ -17,25 +17,42 @@ namespace :waz do
   desc "Sync your assets to Windows Azure, usage : rake waz_sync:sync folders=images,stylesheets,javascripts (you can skip folders arg)"
   task :sync => :app_env do
     
-    folders = ENV['folders'].split(/,/)
+    STDOUT.sync = true
+    
+    folders_tmp = ENV['folders']
 
-    if folders.blank?
+    if folders_tmp.blank?
       folders = ["images", "javascripts", "stylesheets"]      
+    else
+      folders = folders_tmp.split(/,/)
     end
         
     azure = WazSync.new()
+    
     folders.each{|folder|
-
+      
+      puts "  ==> Syncing #{folder}"
+      
       container = azure.find_or_create_container(folder)    
 
+      i = 0
+      Dir.glob("#{Rails.root.to_s}/public/#{folder}/**/*").each do |file|
+        i+=1
+      end
+            
+      j = 0
       Dir.glob("#{Rails.root.to_s}/public/#{folder}/**/*").each do |file|
         if File.file?(file)              
           filename = file.gsub("/public/#{folder}/", "").gsub("#{Rails.root.to_s}", "")
-          p filename
+          print "\r    ==> #{(((j.to_f/i.to_f)*10000).to_i / 100.0).ceil}%"          
           azure.send_or_update(container, file, filename)
         end
+        j+=1
       end
+      print "\n"
     }
+    
+    STDOUT.sync = false
   end
   
 end
